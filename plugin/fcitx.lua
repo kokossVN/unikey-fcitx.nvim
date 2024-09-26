bdf3072598b73afae4cf7cce88360af94049c2ca
Manh Tran <manh12345678951235789@gmail.com>
2024-09-27 05:46:32 +0700

    05:46 Sep 27, 2024

--- plugin/fcitx.lua
+++ plugin/fcitx.lua
@@ -1,5 +1,4 @@
 -- check fcitx-remote (fcitx5-remote)
-print("Hello")
 local fcitx_cmd = ""
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
@@ -44,7 +43,6 @@ end
 
 local function _FcitxInit()
 	if vim.g.vscode then
-		print("Hello")
 	else
 		--create augroup fcitx (but im not sure is does)
 		augroup("fcitx", {
