6c5d41c08f7a0dc48804df89bb6bbb3d226512f6
Kokoss <manh12345678951235789@gmail.com>
2024-11-18 16:40:48 +0700

    feat: add option for ssh server

--- plugin/fcitx.lua
+++ plugin/fcitx.lua
@@ -3,103 +3,111 @@ local fcitx_cmd = ""
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-if vim.fn.executable("fcitx-remote") == 1 then
-	fcitx_cmd = "fcitx-remote"
-elseif vim.fn.executable("fcitx5-remote") == 1 then
-	fcitx_cmd = "fcitx5-remote"
-else
-	return
-end
 
 if os.getenv("SSH_TTY") ~= nil then
-	return
+  function _Fcitx2en()
+    vim.fn.system("echo 'en' >> /tmp/fcitx5-remote.nvim")
+  end
+
+  function _Fcitx2Unikey()
+    vim.fn.system("echo 'vi' >> /tmp/fcitx5-remote.nvim")
+  end
+else
+  if vim.fn.executable("fcitx-remote") == 1 then
+    fcitx_cmd = "fcitx-remote"
+  elseif vim.fn.executable("fcitx5-remote") == 1 then
+    fcitx_cmd = "fcitx5-remote"
+  else
+    return
+  end
+
+  local os_name = vim.loop.os_uname().sysname
+  if
+      (os_name == "Linux" or os_name == "Unix")
+      and os.getenv("DISPLAY") == nil
+      and os.getenv("WAYLAND_DISPLAY") == nil
+  then
+    return
+  end
+
+  function _Fcitx2en()
+    -- input_status return curent input method
+    local input_status = vim.fn.system("fcitx5-remote -n")
+    if input_status ~= "keyboard-us" then
+      -- switch to English
+      vim.fn.system(fcitx_cmd .. " -s keyboard-us")
+    end
+  end
+
+  function _Fcitx2Unikey()
+    local input_status = vim.fn.system("fcitx5-remote -n")
+    if input_status ~= "unikey" then
+      -- switch to Unikey
+      vim.fn.system(fcitx_cmd .. " -s unikey")
+    end
+  end
 end
 
-local os_name = vim.loop.os_uname().sysname
-if
-	(os_name == "Linux" or os_name == "Unix")
-	and os.getenv("DISPLAY") == nil
-	and os.getenv("WAYLAND_DISPLAY") == nil
-then
-	return
-end
-
-function _Fcitx2en()
-	-- input_status return curent input method
-	local input_status = vim.fn.system("fcitx5-remote -n")
-	if input_status ~= "keyboard-us" then
-		-- switch to English
-		vim.fn.system(fcitx_cmd .. " -s keyboard-us")
-	end
-end
-
-function _Fcitx2Unikey()
-	local input_status = vim.fn.system("fcitx5-remote -n")
-	if input_status ~= "unikey" then
-		-- switch to Unikey
-		vim.fn.system(fcitx_cmd .. " -s unikey")
-	end
-end
 
 local function _FcitxInit()
-	--create augroup fcitx (but im not sure is does)
-	augroup("fcitx", {
-		clear = true,
-	})
-	if vim.g.unikey_bydefault == true then
-		autocmd({ "InsertEnter" }, {
-			group = "fcitx",
-			callback = function()
-				_Fcitx2Unikey()
-			end,
-		})
-		autocmd({ "CmdlineEnter" }, {
-			group = "fcitx",
-			pattern = { "[/:\\?]" },
-			callback = function()
-				_Fcitx2Unikey()
-			end,
-		})
-		autocmd({ "InsertLeave" }, {
-			group = "fcitx",
-			callback = function()
-				_Fcitx2en()
-			end,
-		})
-		autocmd({ "CmdlineLeave" }, {
-			group = "fcitx",
-			pattern = { "[/:\\?]" },
-			callback = function()
-				_Fcitx2en()
-			end,
-		})
-	else
-		autocmd({ "CmdlineEnter" }, {
-			group = "fcitx",
-			pattern = { ":" },
-			callback = function()
-				_Fcitx2en()
-			end,
-		})
-		autocmd({ "InsertLeave" }, {
-			group = "fcitx",
-			callback = function()
-				_Fcitx2en()
-			end,
-		})
-		autocmd({ "CmdlineLeave" }, {
-			group = "fcitx",
-			pattern = { "[/:\\?]" },
-			callback = function()
-				_Fcitx2en()
-			end,
-		})
-	end
-	--Insert to Unikey
-	vim.keymap.set("n", "<A-i>", ":lua _Fcitx2Unikey()<CR>i", { silent = true, noremap = true })
+  --create augroup fcitx (but im not sure is does)
+  augroup("fcitx", {
+    clear = true,
+  })
+  if vim.g.unikey_bydefault == true then
+    autocmd({ "InsertEnter" }, {
+      group = "fcitx",
+      callback = function()
+        _Fcitx2Unikey()
+      end,
+    })
+    autocmd({ "CmdlineEnter" }, {
+      group = "fcitx",
+      pattern = { "[/:\\?]" },
+      callback = function()
+        _Fcitx2Unikey()
+      end,
+    })
+    autocmd({ "InsertLeave" }, {
+      group = "fcitx",
+      callback = function()
+        _Fcitx2en()
+      end,
+    })
+    autocmd({ "CmdlineLeave" }, {
+      group = "fcitx",
+      pattern = { "[/:\\?]" },
+      callback = function()
+        _Fcitx2en()
+      end,
+    })
+  else
+    autocmd({ "CmdlineEnter" }, {
+      group = "fcitx",
+      pattern = { ":" },
+      callback = function()
+        _Fcitx2en()
+      end,
+    })
+    autocmd({ "InsertLeave" }, {
+      group = "fcitx",
+      callback = function()
+        _Fcitx2en()
+      end,
+    })
+    autocmd({ "CmdlineLeave" }, {
+      group = "fcitx",
+      pattern = { "[/:\\?]" },
+      callback = function()
+        _Fcitx2en()
+      end,
+    })
+  end
+  --Insert to Unikey
+  vim.keymap.set("n", "<A-i>", ":lua _Fcitx2Unikey()<CR>i", { silent = true, noremap = true })
 end
 
 local init = {
-	start = _FcitxInit(),
+  start = _FcitxInit(),
 }
 return init
